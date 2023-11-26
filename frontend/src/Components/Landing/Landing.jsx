015c7c750bb7f4087b04da0ff0a6ef3980aa5fe4
Saubhagya Singh <76896543+SaubhagyaSingh@users.noreply.github.com>
2023-11-26 14:43:44 +0530

    lannding page frontend done

--- frontend/src/Components/Landing/Landing.jsx
+++ frontend/src/Components/Landing/Landing.jsx
@@ -1,21 +1,6 @@
 import React from "react";
 import vid from "../../Resources/Videos/bgvid.mp4";
 import "../Landing/Landing.css";
-var words = [
-    "N F S",
-    "NEED FOR SECURITY",
-    "Check for Your Digital Footprint",
-    "Stay safe Because",
-    "Privacy is a myth",
-  ],
-  part,
-  i = 0,
-  offset = 0,
-  len = words.length,
-  forwards = true,
-  skip_count = 0,
-  skip_delay = 15,
-  speed = 70;
 
 const Landing = () => {
   return (
@@ -26,10 +11,16 @@ const Landing = () => {
         <h1>MOST WANTED</h1>
         <div className="word"></div>
         <div className="menu">
-          <button className="wheel" id="option1"></button>
-          <button className="wheel" id="option2"></button>
-          <button className="wheel" id="option3"></button>
           <button className="wheel" id="option4"></button>
+          <button className="wheel" id="option2"></button>
+          <button className="wheel" id="option1"></button>
+          <button className="wheel" id="option3"></button>
+          <div className="Options rise">
+            <span>Search</span>
+            <span> About us</span>
+            <span> Vision</span>
+            <span id="lastopt"> Key Features</span>
+          </div>
         </div>
       </div>
     </div>
