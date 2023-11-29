48d31c64727a985a1ed9e2485214796e21c7dccb
Saubhagya Singh <76896543+SaubhagyaSingh@users.noreply.github.com>
2023-11-29 11:49:26 +0530

    working

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../Components/Navbar/Navbar";
 import Card from "../Components/Card/Card";
 import "./About.css";
-
+import Techcard from "../Components/Techcard/Techcard";
 const About = () => {
   return (
     <div>
@@ -13,21 +13,43 @@ const About = () => {
         </div>
       </div>
 
-      <div class=" about section-container">
+      <div className=" about section-container">
         <section>
-          <h1>Each section...</h1>
+          <h1 className="about-heading"> Credits and Thanks</h1>
+          <div>
+            <br />
+            <br />
+            <p>
+              {" "}
+              This project was made as a part of the assignments for AIMLT
+              Course at MIT WPU.
+              <br></br>
+              We are grateful to our teachers, and our parents for encouraging
+              us to keep trying new stuff, without any restrictions.
+              <br></br>
+              None of this would be possible without their help.
+              <br></br>
+              <br></br>
+              We are also grateful to our friends, for their inputs on the looks
+              and features of the website, and for constantly encourating us to
+              keep going.
+            </p>
+          </div>
         </section>
         <section>
-          <h1>Full screen and slanted...</h1>
+          <h1 className="about-heading"> Technology Stack:</h1>
+          <p>
+            <Techcard />
+          </p>
         </section>
         <section>
-          <h1>Go ahead...</h1>
+          <h1 className="about-heading">Go ahead...</h1>
         </section>
         <section>
-          <h1>Keep scrolling...</h1>
+          <h1 className="about-heading">Keep scrolling...</h1>
         </section>
         <section>
-          <h1>Ok, thats it.</h1>
+          <h1 className="about-heading">Ok, thats it.</h1>
         </section>
       </div>
     </div>
